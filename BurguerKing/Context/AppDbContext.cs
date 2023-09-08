bc5001b73300bf35c74279c8783c5b619f7d98f5
J040vitorjv <127050294+J040vitorjv@users.noreply.github.com>
2023-09-08 12:10:34 -0300

    commit inicial

---
+++ BurguerKing/Context/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using BurguerKing.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BurguerKing.Context
+{
+
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+       
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Lanche> Lanches { get; set; }
+
+    }
+}
---
+++ BurguerKing/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BurguerKing.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
